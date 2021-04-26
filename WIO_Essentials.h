7b3c4902c0efc448b3a80a441d9f5c66d27620aa
Wyschy <77229878+Wyschy@users.noreply.github.com>
2021-04-26 19:51:57 +0200

    Add files via upload

--- WIO_Essentials.h
+++ WIO_Essentials.h
@@ -11,6 +11,8 @@ bool KeyR = false;
 bool KeyD = false;
 bool KeyL = false;
 bool KeyP = false;
+int Light;
+int Sonic;
 int Key = 0;
 bool LED = false;
 public:
@@ -26,6 +28,8 @@ void begin() {
   pinMode(WIO_5S_PRESS, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(WIO_BUZZER, OUTPUT);
+  pinMode(WIO_LIGHT, INPUT);
+  pinMode(WIO_MIC, INPUT);
   Key = 0;
   LED = false;
 }
@@ -40,6 +44,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_KEY_A) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_KEY_B) == LOW) {
     Key = 2;
@@ -51,6 +58,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_KEY_B) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_KEY_C) == LOW) {
     Key = 3;
@@ -62,6 +72,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_KEY_C) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_UP) == LOW) {
     Key = 4;
@@ -73,6 +86,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = true;
+    while (digitalRead(WIO_5S_UP) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_DOWN) == LOW) {
     Key = 5;
@@ -84,6 +100,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_5S_DOWN) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_LEFT) == LOW) {
     Key =6 ;
@@ -95,6 +114,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_5S_LEFT) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_RIGHT) == LOW) {
     Key = 7;
@@ -106,6 +128,9 @@ int Keypressed() {
     KeyP = false;
     KeyR = true;
     KeyU = false;
+    while (digitalRead(WIO_5S_RIGHT) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_PRESS) == LOW) {
     Key = 8;
@@ -117,6 +142,9 @@ int Keypressed() {
     KeyP = true;
     KeyR = false;
     KeyU = false;
+    while (digitalRead(WIO_5S_PRESS) == LOW) {
+      delay(10);
+    }
   }
   else Key = 0;
   delay(100);
@@ -127,41 +155,66 @@ int Keyset() {
     Key = 1;
     if (KeyA = true) KeyA = false;
     else KeyA = true;
+    while (digitalRead(WIO_KEY_A) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_KEY_B) == LOW) {
     Key = 2;
     if (KeyB = true) KeyB = false;
     else KeyB = true;
+    while (digitalRead(WIO_KEY_B) == LOW) {
+      delay(10);
+    }
   }
+  
   else if (digitalRead(WIO_KEY_C) == LOW) {
     Key = 3;
     if (KeyC = true) KeyC = false;
     else KeyC = true;
+    while (digitalRead(WIO_KEY_C) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_UP) == LOW) {
     Key = 4;
     if (KeyU = true) KeyU = false;
     else KeyU = true;
+    while (digitalRead(WIO_5S_UP) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_DOWN) == LOW) {
     Key = 5;
     if (KeyD = true) KeyD = false;
     else KeyD = true;
+    while (digitalRead(WIO_5S_DOWN) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_LEFT) == LOW) {
     Key =6 ;
     if (KeyL = true) KeyL = false;
     else KeyL = true;
+    while (digitalRead(WIO_5S_LEFT) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_RIGHT) == LOW) {
     Key = 7;
     if (KeyR = true) KeyR = false;
     else KeyR = true;
+        while (digitalRead(WIO_5S_RIGHT) == LOW) {
+      delay(10);
+    }
   }
   else if (digitalRead(WIO_5S_PRESS) == LOW) {
     Key = 8;
     if (KeyP = true) KeyP = false;
     else KeyP = true;
+    while (digitalRead(WIO_5S_PRESS) == LOW) {
+      delay(10);
+    }
   }
   else Key = 0;
   delay(100);
@@ -230,5 +283,20 @@ void buzzer(int leng)  {
   delay(leng);
   analogWrite(WIO_BUZZER, 0);
   delay(leng);
+}
+int meassureLight() {
+   Light = analogRead(WIO_LIGHT);
+   delay(200);
+   return Light;
+}
+int getLight() {
+  return Light;
+}
+int meassureSonic() {
+  Sonic = analogRead(WIO_MIC);
+  return Sonic;
+}
+int getSonic() {
+  return Sonic;
 }
 };
